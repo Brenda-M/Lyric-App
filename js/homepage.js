23446d6dca5dc094532ab10deafd7c2d101f28cc
Brenda-M <brendanjuguna1@gmail.com>
2020-03-17 22:08:46 +0300

    use fetch to search songs by artist and title

--- js/homepage.js
+++ js/homepage.js
@@ -2,3 +2,16 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+async function searchSongs(term) {
+	const response = await fetch(`${apiURL}/suggest/${term}`;)
+	const data = await response.json();
+
+	showData(data);
+}
+
+function showData(data){
+	result.innerHTML = ``
+}
