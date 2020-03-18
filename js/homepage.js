c49b283159e5659669413464cdafea4a9eec4461
Brenda-M <brendanjuguna1@gmail.com>
2020-03-18 15:45:25 +0300

    create function for searching songs

--- js/homepage.js
+++ js/homepage.js
@@ -8,6 +8,18 @@ const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
 
+//search by song or artist
+async function searchSongs(term){
+	const res = await fetch(`${apiURL}/suggest/${term}`);
+	const data = await res.json();
+
+	showData(data);
+}
+
+//diplay search results in the DOM
+
+
+
 //Event Listeners
 form.addEventListener('submit', e => {
 	e.preventDefault();
@@ -24,24 +36,4 @@ if(!searchTerm){
 
 });
 
-// async function searchSongs(term) {
-// 	const response = await fetch(`${apiURL}/suggest/${term}`;)
-// 	const data = await response.json();
 
-// 	showData(data);
-// }
-
-// function showData(data){
-// 	result.innerHTML = `<ul class="songs>
-// 	${data.data
-// 		.map(
-// 			song => `<li>
-// 			<span><strong>${song.artist.name}</strong> - ${song.title}</span>
-// 			<button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-// 			</li>`
-// 		)
-// 		.join('')}
-// 	</ul>`;
-
-	
-// }
