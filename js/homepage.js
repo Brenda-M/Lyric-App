b36c5aa92efb0b89dbcaf9ec2340f443b4e20673
Brenda-M <brendanjuguna1@gmail.com>
2020-03-18 17:03:27 +0300

    javascript code

--- js/homepage.js
+++ js/homepage.js
@@ -13,5 +13,16 @@ async function searchSongs(term) {
 }
 
 function showData(data){
-	result.innerHTML = ``
+	result.innerHTML = `<ul class="songs>
+	${data.data
+		.map(
+			song => `<li>
+			<span><strong>${song.artist.name}</strong> - ${song.title}</span>
+			<button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+			</li>`
+		)
+		.join('')}
+	</ul>`;
+
+	
 }
