2e3e0b6cd33217380170b59eb18a475099dec27a
Brenda-M <brendanjuguna1@gmail.com>
2020-03-18 15:28:09 +0300

    add eveventlisteners for search button

--- js/homepage.js
+++ js/homepage.js
@@ -1,28 +1,47 @@
+//we making  two requests to the api. 1. get a list of songs when we type search button
+//2. when we press get lyrics button it makes another request to the api with the song and artist name
+
 const form = document.getElementById('form');
-const search = document.getElementById('search');
+const search = document.getElementById('searchMusic');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
 
-async function searchSongs(term) {
-	const response = await fetch(`${apiURL}/suggest/${term}`;)
-	const data = await response.json();
+//Event Listeners
+form.addEventListener('submit', e => {
+	e.preventDefault();
 
-	showData(data);
+	const searchTerm = search.value.trim();
+
+//validation to ensure user typed something in search box
+
+if(!searchTerm){
+	alert("Please enter a search term");
+} else {
+	searchSongs(searchTerm);
 }
 
-function showData(data){
-	result.innerHTML = `<ul class="songs>
-	${data.data
-		.map(
-			song => `<li>
-			<span><strong>${song.artist.name}</strong> - ${song.title}</span>
-			<button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-			</li>`
-		)
-		.join('')}
-	</ul>`;
+});
+
+// async function searchSongs(term) {
+// 	const response = await fetch(`${apiURL}/suggest/${term}`;)
+// 	const data = await response.json();
+
+// 	showData(data);
+// }
+
+// function showData(data){
+// 	result.innerHTML = `<ul class="songs>
+// 	${data.data
+// 		.map(
+// 			song => `<li>
+// 			<span><strong>${song.artist.name}</strong> - ${song.title}</span>
+// 			<button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+// 			</li>`
+// 		)
+// 		.join('')}
+// 	</ul>`;
 
 	
-}
+// }
